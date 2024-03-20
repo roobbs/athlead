3194990d609fd8c6800be6b4a6297727eb9ec208
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-03-20 16:07:43 -0600

    Added main structure of cartScreen

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 import productsList from "../../products.js";
 import Router from "../Router.jsx";
 
@@ -7,16 +7,33 @@ export const ShopContext = createContext({
   products: [],
   cartItems: [],
   addToCart: () => {},
+  favItems: [],
+  addToFav: () => {},
 });
 
 export default function App() {
   const [cartItems, setCartItems] = useState([]);
+  const [favItems, setFavItems] = useState([]);
   const products = productsList;
-  const addToCart = (product) =>
+  const addToCart = (product) => {
     setCartItems((prevState) => [...prevState, product]);
+  };
+  const addToFav = (product) => {
+    setFavItems((prevState) => [...prevState, product]);
+  };
+
+  //DELETE useEffect
+  useEffect(() => {
+    addToCart(products[1]);
+    addToCart(products[2]);
+    addToFav(products[2]);
+  }, []);
+  console.log(cartItems);
 
   return (
-    <ShopContext.Provider value={{ cartItems, products, addToCart }}>
+    <ShopContext.Provider
+      value={{ products, cartItems, addToCart, favItems, addToFav }}
+    >
       <Router />
     </ShopContext.Provider>
   );
--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -1,13 +1,67 @@
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import "../styles/CartScreen.css";
+import { useContext } from "react";
+import { ShopContext } from "../App/App";
+import ProductCard from "../components/ProductCard";
+import arrow from "../assets/right-arrow.png";
 
 export default function CartScreen() {
+  const { cartItems, favItems } = useContext(ShopContext);
+
   return (
     <>
-      <div>hola</div>
-      <div>CART</div>
-      <div>
-        <Link to="/"> Seguir Comprando</Link>
+      <div className="cartScreenContainer">
+        <div className="cartProductsInfo">
+          <div className="cartTitles">TU CARRITO</div>
+          <div style={{ color: "grey", fontStyle: "italic" }}>
+            TOTAL ({cartItems.length} productos)
+          </div>
+          <div>
+            Los articulos en tu carrito no estan reservados. Termina el proceso
+            de compra ahora para hacerte con ellos.
+          </div>
+          <div>MAP PRODUCTOS</div>
+        </div>
+        <div className="cartResume">
+          <Link to="/" className="keepPayingButton">
+            <div>SEGUIR COMPRANDO</div>
+            <img src={arrow} alt="" className="buyIcon" />
+          </Link>
+          <div className="cartPayButtonContainer">
+            <div className="cartPayButton">
+              <div>IR A PAGAR</div>
+              <img src={arrow} alt="" className="payIcon" />
+            </div>
+          </div>
+          <div className="resumeTitle">RESUMEN DEL PEDIDO</div>
+          <div>
+            <div>{cartItems.length} productos</div>
+            <div>Entrega Gratis</div>
+          </div>
+          <div>
+            <div>Total</div>
+            <div>$precio</div>
+            <div>(IVA incluido)</div>
+          </div>
+          <div>OPCIONES DE PAGO</div>
+          <div>LISTA DE OPCIONES</div>
+        </div>
+        <div className="favOnCartContainer">
+          <div className="cartTitles">TUS FAVORITOS</div>
+          <div className="favItemsContainer">
+            {favItems.map((product) => (
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                category={product.category}
+                label={product.label}
+                img={product.image}
+              />
+            ))}
+          </div>
+        </div>
       </div>
       <Footer />
     </>
