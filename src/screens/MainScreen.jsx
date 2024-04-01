e9453ba55da1eb6c5b20d11950ea4177e7013862
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-04-01 14:44:32 -0600

    Added pictures and buttons on mainScreen

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,13 +1,33 @@
 import "../styles/MainScreen.css";
 import { Link } from "react-router-dom";
+import WhiteButton from "../components/WhiteButton";
+import all from "../images/all.jpg";
+import men from "../images/men.jpg";
+import women from "../images/women.jpg";
 
 export default function MainScreen() {
   return (
-    <>
-      <div>hola</div>
-      <Link to="all">Ver todo el calzado</Link>
-      <Link to="women">Ver el calzado para mujer</Link>
-      <Link to="men">Ver el calzado para hombre</Link>
-    </>
+    <div className="mainScreenContainer">
+      <div className="container">
+        <img src={all} alt="mainPic" className="mainPic" />
+        <Link to="all" className="mainScreenLink">
+          <WhiteButton text="Ver todo el calzado" />
+        </Link>
+      </div>
+      <div className="secondContainer">
+        <div className="container">
+          <img src={men} alt="" className="mainPic" />
+          <Link to="men" className="mainScreenLink">
+            <WhiteButton text="Comprar calzado para Hombre" />
+          </Link>
+        </div>
+        <div className="container">
+          <img src={women} alt="" className="mainPic" />
+          <Link to="women" className="mainScreenLink">
+            <WhiteButton text="Comprar calzado para mujer" />
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
