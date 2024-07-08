4f13d52619a244eac400b94c3cd739afbc7dc944
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-08 16:45:47 -0600

    Created new style for ProductScreen for small screens

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -22,34 +22,86 @@ export default function ProductScreen() {
   }, [favItems, isFav, selectedProduct]);
 
   return (
-    <div className="productScreenContainer">
-      <div className="productScreenContent">
-        <div className="productImagesContainer">
-          <img src={selectedProduct.images[0]} alt="productImage" />
-          <img src={selectedProduct.images[1]} alt="productImage" />
-          <img src={selectedProduct.images[2]} alt="productImage" />
-          <img src={selectedProduct.images[3]} alt="productImage" />
+    <>
+      <div className="productScreenContainer">
+        <div className="productScreenContent">
+          <div className="productImagesContainer">
+            <img src={selectedProduct.images[0]} alt="productImage" />
+            <img src={selectedProduct.images[1]} alt="productImage" />
+            <img src={selectedProduct.images[2]} alt="productImage" />
+            <img src={selectedProduct.images[3]} alt="productImage" />
+          </div>
+          <div>
+            <DisplayInfo title={"Descripcion"}>
+              <h3>{selectedProduct.description[0]}</h3>
+              <div>{selectedProduct.description[1]}</div>
+            </DisplayInfo>
+            <DisplayInfo title={"Detalles"}>
+              <ul>
+                {selectedProduct.specifications.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </DisplayInfo>
+          </div>
         </div>
-        <div>
-          <DisplayInfo title={"Descripcion"}>
-            <h3>{selectedProduct.description[0]}</h3>
-            <div>{selectedProduct.description[1]}</div>
-          </DisplayInfo>
-          <DisplayInfo title={"Detalles"}>
-            <ul>
-              {selectedProduct.specifications.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-          </DisplayInfo>
+        <div className="productScreenSideBar">
+          <div className="productScreenCategory">
+            ● {selectedProduct.category}
+          </div>
+          <div className="productScreenName">{selectedProduct.name}</div>
+          <div className="productScreenPrice">${selectedProduct.price}</div>
+          <div>{selectedProduct.label}</div>
+          <div className="prodScreenButtonsContainer">
+            <div className="cartPayButtonContainer">
+              <div
+                className="cartPayButton"
+                onClick={() => addToCart(selectedProduct)}
+              >
+                <div>Agregar al carrito</div>
+                <img src={arrow} alt="" className="payIcon" />
+              </div>
+            </div>
+            <img
+              src={isFav ? favIcon1 : favIcon}
+              alt="favIcon"
+              className="productScreenFavIcon"
+              onClick={() => {
+                if (!isFav) {
+                  addToFav(selectedProduct);
+                } else {
+                  eraseFav(selectedProduct);
+                }
+              }}
+            />
+          </div>
+          <div style={{ display: "flex", gap: 20, flexDirection: "column" }}>
+            <div className="productScreenInfoContainer">
+              <img src={entrega} alt="" className="infoIcon" />
+              <div>ENVIOS A PARTIR DE $1199</div>
+            </div>
+            <div className="productScreenInfoContainer">
+              <img src={check} alt="" className="infoIcon" />
+              <div>COMUNICATE CON NOSOTROS</div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="productScreenSideBar">
+      {/* mobile */}
+      <div className="productScreenContainerMobile">
         <div className="productScreenCategory">
           ● {selectedProduct.category}
         </div>
         <div className="productScreenName">{selectedProduct.name}</div>
         <div className="productScreenPrice">${selectedProduct.price}</div>
+        <div className="productScreenContent">
+          <div className="productImagesContainer">
+            <img src={selectedProduct.images[0]} alt="productImage" />
+            <img src={selectedProduct.images[1]} alt="productImage" />
+            <img src={selectedProduct.images[2]} alt="productImage" />
+            <img src={selectedProduct.images[3]} alt="productImage" />
+          </div>
+        </div>
         <div>{selectedProduct.label}</div>
         <div className="prodScreenButtonsContainer">
           <div className="cartPayButtonContainer">
@@ -74,6 +126,19 @@ export default function ProductScreen() {
             }}
           />
         </div>
+        <div>
+          <DisplayInfo title={"Descripcion"}>
+            <h3>{selectedProduct.description[0]}</h3>
+            <div>{selectedProduct.description[1]}</div>
+          </DisplayInfo>
+          <DisplayInfo title={"Detalles"}>
+            <ul>
+              {selectedProduct.specifications.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </DisplayInfo>
+        </div>
         <div style={{ display: "flex", gap: 20, flexDirection: "column" }}>
           <div className="productScreenInfoContainer">
             <img src={entrega} alt="" className="infoIcon" />
@@ -85,6 +150,6 @@ export default function ProductScreen() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
