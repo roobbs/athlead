a989f80ea60572371deb217a684bfa978e196cbf
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-09 21:50:38 -0600

    Added new main screen structure

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,32 +1,26 @@
 import "../styles/MainScreen.css";
 import { Link } from "react-router-dom";
 import WhiteButton from "../components/WhiteButton";
-import all from "../images/women.webp";
-import all2 from "../images/men.webp";
+import all from "../images/all.jpg";
 import men from "../images/samba.webp";
 import women from "../images/wom.webp";
 
 export default function MainScreen() {
   return (
     <div className="mainScreenContainer">
+      <div className="topBar">
+        <img src={all} alt="mainPic" className="mainPic" />
+        <Link to="all" className="mainScreenLink">
+          <WhiteButton text="Ver todo el calzado" />
+        </Link>
+      </div>
+      <div>Animation here</div>
       <div className="secondContainer">
-        <div className="container">
-          <img src={all} alt="mainPic" className="mainPic" />
-          <Link to="all" className="mainScreenLink">
-            <WhiteButton text="Ver todo el calzado" />
-          </Link>
-        </div>
         <div className="container">
           <img src={women} alt="" className="mainPic" />
           <Link to="women" className="mainScreenLink">
             <WhiteButton text="Comprar calzado para Mujer" />
           </Link>
-        </div>
-        <div className="container">
-          <img src={all2} alt="mainPic" className="mainPic" />
-          {/* <Link to="all" className="mainScreenLink">
-            <WhiteButton text="Ver todo el calzado" />
-          </Link> */}
         </div>
         <div className="container">
           <img src={men} alt="" className="mainPic" />
