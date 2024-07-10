8657394f8bc2f7012df9612e19f719ce2465236a
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-07-09 22:25:13 -0600

    Added top message on MainScreen.jsx

--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -4,17 +4,31 @@ import WhiteButton from "../components/WhiteButton";
 import all from "../images/all.webp";
 import men from "../images/samba.webp";
 import women from "../images/wom.webp";
+import { IoClose } from "react-icons/io5";
 
 export default function MainScreen() {
   return (
     <div className="mainScreenContainer">
+      <input type="checkbox" id="cuponCheckbox" />
+      <div className="cuponContainer">
+        <div>15% EXTRA usando el código 🎫ATHLEAD15🎫</div>
+        <label htmlFor="cuponCheckbox">
+          <IoClose size={30} />
+        </label>
+      </div>
       <div className="topBar">
         <img src={all} alt="mainPic" className="mainPic" />
         <Link to="all" className="mainScreenLink">
           <WhiteButton text="Ver todo el calzado" />
         </Link>
       </div>
-      <div>Animation here</div>
+      {/* <div className="animationContainer">
+        <div className="animationTitle">🛒FINAL DE TEMPORADA 🛒</div>
+        <div>
+          Usa el código 🎫ATHLEAD15🎫 y obtén 15% OFF adicional en tus compras
+          de final de temporada
+        </div>
+      </div> */}
       <div className="secondContainer">
         <div className="container">
           <img src={women} alt="" className="mainPic" />
